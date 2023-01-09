aa110d054bb987ce7816f49e77f2a92a6cc1b888
kumattau <kumattau@gmail.com>
2023-01-09 13:56:36 +0900

    Add init/fini of screen

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,36 @@
+use std::io::stdout;
+
 use anyhow::Result;
+use crossterm::{
+    execute,
+    terminal::{self, EnterAlternateScreen, LeaveAlternateScreen},
+};
+
+#[derive(Debug, Default)]
+struct Screen;
+
+impl Screen {
+    fn init() -> Result<()> {
+        let hook = std::panic::take_hook();
+        std::panic::set_hook(Box::new(move |info| {
+            _ = Screen::fini();
+            hook(info);
+        }));
+        terminal::enable_raw_mode()?;
+        execute!(stdout(), EnterAlternateScreen)?;
+        Ok(())
+    }
+
+    fn fini() -> Result<()> {
+        execute!(stdout(), LeaveAlternateScreen)?;
+        terminal::disable_raw_mode()?;
+        Ok(())
+    }
+}
 
 fn main() -> Result<()> {
+    Screen::init()?;
+    Screen::fini()?;
     Ok(())
 }
 
