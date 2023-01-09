4b070c193c226ed9eb6334a753f6bafc9f32269d
kumattau <kumattau@gmail.com>
2023-01-10 00:09:03 +0900

    Add draw

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,40 @@
 use std::{
     fs::File,
-    io::stdout,
+    io::{stdout, Write},
     path::{Path, PathBuf},
 };
 
 use anyhow::Result;
 use clap::Parser;
 use crossterm::{
+    cursor,
     event::{self, Event, KeyCode, KeyEvent, KeyModifiers},
-    execute,
+    execute, queue,
+    style::Print,
     terminal::{self, EnterAlternateScreen, LeaveAlternateScreen},
 };
 use ropey::Rope;
+use unicode_segmentation::UnicodeSegmentation;
+use unicode_width::UnicodeWidthStr;
 
 #[derive(Debug, Parser)]
 #[clap(name = env!("CARGO_PKG_NAME"), version, author, about)]
 struct Opts {
     path: PathBuf,
+}
+
+fn is_linebreak(str: &str) -> bool {
+    matches!(
+        str,
+        "\r\n"
+            | "\u{000A}"
+            | "\u{000B}"
+            | "\u{000C}"
+            | "\u{000D}"
+            | "\u{0085}"
+            | "\u{2028}"
+            | "\u{2029}"
+    )
 }
 
 #[derive(Debug, Default)]
@@ -56,6 +74,48 @@ impl Screen {
         terminal::disable_raw_mode()?;
         Ok(())
     }
+
+    fn size() -> Result<(usize, usize)> {
+        let (cols, rows) = terminal::size()?;
+        Ok((cols as _, rows as _))
+    }
+
+    fn draw(buffer: &Buffer) -> Result<()> {
+        let mut stdout = stdout();
+
+        let (cols, rows) = Screen::size()?;
+
+        queue!(stdout, cursor::Hide)?;
+        queue!(stdout, terminal::Clear(terminal::ClearType::All))?;
+        queue!(stdout, cursor::MoveTo(0, 0))?;
+
+        let mut row = 0;
+        'outer: for line in buffer.rope.lines() {
+            let mut col = 0;
+            for segm in line.to_string().graphemes(true) {
+                if !is_linebreak(segm) {
+                    col += segm.width();
+                    if col >= cols {
+                        col = 0;
+                        row += 1;
+                        if row >= rows {
+                            break 'outer;
+                        }
+                    }
+                    queue!(stdout, Print(segm))?;
+                }
+            }
+            row += 1;
+            if row >= rows {
+                break 'outer;
+            }
+            queue!(stdout, Print("\r\n"))?;
+        }
+
+        queue!(stdout, cursor::Show)?;
+        stdout.flush()?;
+        Ok(())
+    }
 }
 
 fn main() -> Result<()> {
@@ -69,6 +129,8 @@ fn main() -> Result<()> {
 
     Screen::init()?;
     loop {
+        Screen::draw(&buffer)?;
+
         #[allow(clippy::single_match)]
         #[allow(clippy::collapsible_match)]
         match event::read()? {
